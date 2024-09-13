e6f3f165dfbac32752a68bc33f9b8f68b15c25aa
aman <amankrsingh.2012@gmail.com>
2024-09-13 19:15:24 +0530

    First Commit

---
+++ src/main/java/org/scaler/splitwise5junelld/SplitWise5JuneLldApplication.java
@@ -0,0 +1,13 @@
+package org.scaler.splitwise5junelld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SplitWise5JuneLldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SplitWise5JuneLldApplication.class, args);
+    }
+
+}
